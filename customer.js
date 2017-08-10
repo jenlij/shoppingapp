a355cb2fccf80fb7729a2d218a4fd5ff5e7571d7
jenlij <jljohns1216@gmail.com>
2017-08-10 11:55:14 -0400

    writing tests for customer methods

--- customer.js
+++ customer.js
@@ -1,4 +1,42 @@
 require('dotenv').config();
+const pg = require('pg-promise')();
+const dbConfig = {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME
+};
+// const db = pg(dbConfig);
+class Customer {
+    constructor(name, email, addr, pw) {
+        this.db = pg(dbConfig);
+        this.name = name;
+        this.email = email;
+        this.address = addr;
+        this.password = pw;
+    }
+    save() {
+        return this.db.query(`
+        insert into customers
+        (name, email, address, password)
+        values
+        ('${this.name}', '${this.email}', '${this.address}', '${this.password}');
+        `);
+    }
+    get(id){
+        return this.db.one(`
+        SELECT * FROM customers WHERE customer_id = ${id};
+        `).then((result) => {
+            this.name = result.name;
+            return result;
+        });
+    }
+
+}
+module.exports = Customer;
+
+
+//npm run test-watch
+
 
 // var db = require('db')
 // db.connect({
--- test/test-customer.js
+++ test/test-customer.js
@@ -1,8 +1,26 @@
-
 const expect = require('chai').expect;
+const Customer = require('../customer');
 
 describe('Customers', () => {
-    it('should be able to save to database', () => {
-        expect(1+1).to.equal(2);
+    it('should be able to save to database', (done) => {
+        let myCustomer = new Customer('me', 'me@me.com', '123 me street', 'm3m3m3');
+        myCustomer
+            .save()
+            .then((result) => {
+                done();
+            });
     });
-})
+    it('should be able to get a customer from the database', (done) => {
+        let myCustomer = new Customer();
+        myCustomer
+            .get(1)
+            .then((result) =>{
+                console.log(result);
+                done();
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+
+    });    
+});
