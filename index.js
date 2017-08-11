d9ce5d14a0f70ce0135968286003a4c2947b5e2d
jenlij <jljohns1216@gmail.com>
2017-08-11 09:50:20 -0400

    tests order of execution

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 - write code to fulfill test
 */
 
-require('dotenv').config();
+// require('dotenv').config();
 
 // var db = require('db')
 // db.connect({
@@ -14,3 +14,22 @@ require('dotenv').config();
 //   username: process.env.DB_USER,
 //   password: process.env.DB_PASS
 // })
+//open chrome://inspect/#devices
+//node --inspect-brk index.js
+
+const express = require('express');
+const webapp = express();  //for get/post requests
+const Customer = require('./customer');
+webapp.get('/', (req, resp) => {
+    Customer
+    .get(1)
+    .then((myCustomer) => {
+        resp.end(JSON.stringify(myCustomer));
+    }).catch((err) => {
+        resp.end('sorry!')
+    });
+});
+
+webapp.listen(5000, () => {
+    console.log('hellooooo');
+})
--- test/test-customer.js
+++ test/test-customer.js
@@ -2,14 +2,28 @@ const expect = require('chai').expect;
 const Customer = require('../customer');
 
 describe('Customers', () => {
+//     console.log('one');
+//   it('reads the contents of a file', (done) => {
+//     console.log('two');
+//     fileManipulator.readFile('hello-world.txt', (contents) => {
+//       console.log('three');
+//       expect(contents).to.equal('hello world\n');
+//       done();
+//     });
+//   });
+//   console.log('four');
+    // console.log('one');
     it('should be able to save to database', (done) => {
+        // console.log('two');
         let myCustomer = new Customer('me', 'me@me.com', '123 me street', 'm3m3m3');
         myCustomer
             .save()
             .then((result) => {
+                // console.log('three');
                 done();
             });
     });
+        // console.log('four');
     it('should be able to get a customer from the database', (done) => {
         Customer
             .get(1)
@@ -56,25 +70,29 @@ describe('Customers', () => {
                         .catch(console.log);
          });
     });
-    it('should update a user and retain new values', (done) => {
-        // customer has id? if so, update. if not, save new.
-        let newName = 'Diana Prince';
-        let data = ['wonder woman', 'diana@themyscira', 'themyscira', 'justice'];
-        let c1 = new Customer(...data);
-        c1.save()
-                .then((resultFromSave1) => {
-                    c1.name = newName;
-                    c1.save()
-                        .then((resultFromSave2) => {
-                            let customer_id = resultFromSave1.customer_id;
-                            Customer.get(customer_id)
-                                .then((c2) => {
-                                    expect(c2.name).to.equal(newName);
-                                     done();
-                                })
-                                .catch(console.log);
-                        })
-                })      
-         });
+    // it('should update a user and retain new values', (done) => {
+    //     // customer has id? if so, update. if not, save new.
+    //     let newName = 'Diana Prince';
+    //     let data = ['wonder woman', 'diana@themyscira', 'themyscira', 'justice'];
+    //     let c1 = new Customer(...data);
+    //     c1.save()
+    //             .then((resultFromSave1) => {
+    //                 c1.name = newName;
+    //                 c1.save()
+    //                     .then((resultFromSave2) => {
+    //                         let customer_id = resultFromSave1.customer_id;
+    //                         Customer.get(customer_id)
+    //                             .then((c2) => {
+    //                                 expect(c2.name).to.equal(newName);
+    //                                  done();
+    //                             })
+    //                             .catch(console.log);
+    //                     })
+    //             })      
+    //      });
+
+
+
+
     });    
        
